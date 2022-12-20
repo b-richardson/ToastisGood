231e5dd20a5b280abb12f03136b64b9e425f9629
Brian Richardson <97001156+b-richardson@users.noreply.github.com>
2022-12-20 15:15:47 -0500

    Convert number(interger) to string

---
+++ 12-20 convert num to string.js
@@ -0,0 +1,10 @@
+// We need a function that can transform a number (integer) into a string.
+
+// What ways of achieving this do you know?
+
+function numberToString(num) {
+    let bob = num.toString()
+    
+    console.log(typeof bob)
+    return bob
+  }
