10528517d1c7ed4f7437323e9c033bcd229484b0
Brian Richardson <97001156+b-richardson@users.noreply.github.com>
2023-02-15 14:31:53 -0500

    more practice

---
+++ 2-15LCb.js
@@ -0,0 +1,20 @@
+// Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+
+ 
+
+var missingNumber = function(nums) {
+    let sorted = nums.sort()
+    console.log(sorted)
+    console.log(sorted[0])
+    console.log(sorted[1] + 1)
+
+    for(let i = 0; i < nums.length - 1; i++){
+        if(sorted[i + 1] == sorted[i] + 1){
+            console.log('good to go')
+        }else{
+            return (sorted[i] + 1)
+        }
+    }
+    return nums.length
+
+};
